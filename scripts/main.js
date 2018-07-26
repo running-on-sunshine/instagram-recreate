646c3728658a68cfca00848f3dffe3c9e52e5eb5
Liz Khuu <khuu.liz@gmail.com>
2018-07-26 10:52:21 -0400

    cleaned up some lines from the script file

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,5 @@
-var images;
-var container;
-var lightBoxContainer;
-var lightBox;
-var lightBoxImage;
-var lightBoxCaption;
-var exitButton;
-var i;
-var openLightBox;
-var backArrow;
-var forwardArrow;
-
 // Landscape-themed-images
-images = [
+var images = [
     { caption: "Caption1", index: 0, src: "aventuras/billy-williams-262740-unsplash.jpg" },
     { caption: "Caption2", index: 1, src: "aventuras/brianda-maldonado-741341-unsplash.jpg" },
     { caption: "Caption3", index: 2, src: "aventuras/idan-arad-109776-unsplash.jpg" },
@@ -24,28 +12,24 @@ images = [
     
 ];
 
-container = document.querySelector('.image-thumbnail-container');
-lightBoxContainer = document.querySelector('.lightbox-container');
-lightBox = document.querySelector('.lightbox');
-lightBoxImage = document.querySelector('.lightbox-image');
-lightBoxCaption = document.querySelector('.lightbox-caption');
-exitButton = document.querySelector('.exit-button');
-backArrow = document.querySelector('.left-arrow');
-forwardArrow = document.querySelector('.right-arrow');
+var container = document.querySelector('.image-thumbnail-container');
+var lightBoxContainer = document.querySelector('.lightbox-container');
+var lightBox = document.querySelector('.lightbox');
+var lightBoxImage = document.querySelector('.lightbox-image');
+var lightBoxCaption = document.querySelector('.lightbox-caption');
+var exitButton = document.querySelector('.exit-button');
+var backArrow = document.querySelector('.left-arrow');
+var forwardArrow = document.querySelector('.right-arrow');
 
 var currentImageIndex;
 
-for (i = 0; i < images.length; i++) {
+for (var i = 0; i < images.length; i++) {
     (function() {
-        var image;
-        var newImage;
-        var imageThumbnail;
-
         // Copies 'i' every time it creates a new desk space
         var currentI = i;
 
-        image = images[i];
-        newImage = document.createElement('img');
+        var image = images[i];
+        var newImage = document.createElement('img');
         newImage.setAttribute('src', image.src);
         newImage.classList.add('image');
 
@@ -53,20 +37,20 @@ for (i = 0; i < images.length; i++) {
         // caption.textContent = image.caption;
         // caption.classList.add('image-caption');
 
-        imageThumbnail = document.createElement('li');
+        var imageThumbnail = document.createElement('li');
         imageThumbnail.classList.add('image-thumbnail');
         // imageThumbnail.setAttribute('data-index', image.index);
-        imageThumbnail.setAttribute('data-index', i);
+        // imageThumbnail.setAttribute('data-index', i);
 
         imageThumbnail.appendChild(newImage);
         // imageThumbnail.appendChild(caption);    
         
-        openLightBox = function (event) {
+        var openLightBox = function (event) {
             currentImageIndex = currentI;
             changeImage(currentImageIndex);
             // console.log(currentI);
             changeCaption(currentImageIndex);
-            lightBox.classList.remove('hide-lightbox');
+            // lightBox.classList.remove('hide-lightbox');
             lightBoxContainer.classList.remove('hide-lightbox');
         };
 
@@ -80,13 +64,19 @@ var changeImage = function (currentImageIndex) {
     lightBoxImage.setAttribute('src', images[currentImageIndex].src);
 };
 
-var changeCaption = function(currentImageIndex) {
+var changeCaption = function (currentImageIndex) {
     lightBoxCaption.textContent = images[currentImageIndex].caption;
 };
 
+
 var closeLightBox = function () {
     lightBoxContainer.classList.add('hide-lightbox');
-    lightBox.classList.add('hide-lightbox');
+    // lightBox.classList.add('hide-lightbox');
+};
+
+var updateLightBox = function () {
+    changeImage(currentImageIndex);
+    changeCaption(currentImageIndex);
 };
 
 var goBackImage = function () {
@@ -94,8 +84,7 @@ var goBackImage = function () {
     if (currentImageIndex < 0) {
         currentImageIndex = images.length-1;
     }
-    changeImage(currentImageIndex);
-    changeCaption(currentImageIndex);
+    updateLightBox(currentImageIndex);
 };
 
 var goForwardImage = function () {
@@ -103,8 +92,7 @@ var goForwardImage = function () {
     if (currentImageIndex > images.length) {
         currentImageIndex = 0;
     }
-    changeImage(currentImageIndex);
-    changeCaption(currentImageIndex);
+    updateLightBox(currentImageIndex);
 };
 
 // Exit Button Listeners
